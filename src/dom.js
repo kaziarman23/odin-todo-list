b6c8fd4921c45a8064d0117e555685c51a59591c
kaziarman23 <kaziarman@proton.me>
2024-03-11 01:45:18 +0600

    added event on delete btn

--- src/dom.js
+++ src/dom.js
@@ -2,41 +2,88 @@ const formCreateBtn = document.querySelector(".formCreateBtn");
 const formOne = document.querySelector(".formOne");
 const mainContent = document.querySelector(".mainContent");
 
+const formTwo = document.querySelector(".formTwo");
+const formTwoSaveBtn = document.querySelector(".formTwoSaveBtn");
+const detailsBtn = document.querySelector(".detailsBtn");
 
 function addContent() {
     formCreateBtn.addEventListener("click", function () {
         formOne.style.display = "none";
-        const pageContent = document.createElement("div")
-       
-        const detailsBtn = document.createElement("button")
-        detailsBtn.innerHTML = `Details`
-            // detailsBtn.addEventListener("click",function(){
-            //     detailsBtn.remove()
+        const pageContent = document.createElement("div");
 
-            // })
+        const detailsBtn = document.createElement("button");
+        detailsBtn.innerHTML = `Details`;
 
+        // adding event on details btn
+        detailsBtn.addEventListener("click", function () {
+            detailsBtn.remove();
+            formTwo.style.display = "block";
 
-        const nameInput = document.querySelector("#nameInput").value
-        let titel = document.createElement("h1")
-        titel.innerHTML =`${nameInput}`
+            formTwoSaveBtn.addEventListener("click", function () {
+                const dateInput = document.querySelector("#dateInput").value;
+                let date = document.createElement("p");
+                date.innerHTML = `Due date: ${dateInput}`;
 
-        const deleteBtn = document.createElement("button")
-        deleteBtn.innerHTML= `Delete`
-        // have to add event listener
-        
+                const priorityInput =
+                    document.querySelector("#priorityInput").value;
+                if (priorityInput === "Low") {
+                    pageContent.style.borderBottom = "5px solid green";
+                } else if (priorityInput === "Medium") {
+                    pageContent.style.borderBottom = "5px solid yellow";
+                } else {
+                    pageContent.style.borderBottom = "5px solid red";
+                }
+
+                const discriptionInput = document.querySelector("#discriptionInput").value;
+                let discription = document.createElement("p");
+                discription.innerHTML = `discription: ${discriptionInput}`;
+
+                const detailsDiv = document.createElement("div");
+
+                // add class
+                detailsDiv.setAttribute("class", "detailsDiv");
+                date.setAttribute("class", "date");
+                discription.setAttribute("class", "discription");
+
+                // appending childs
+                detailsDiv.appendChild(date);
+                detailsDiv.appendChild(discription);
+                pageContent.appendChild(detailsDiv)
+
+                formTwo.style.display = "none";
+                clearInput();
+            });
+        });
+
+        const nameInput = document.querySelector("#nameInput").value;
+        const titel = document.createElement("h1");
+        titel.innerHTML = `${nameInput}`;
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerHTML = `Delete`;
+            deleteBtn.addEventListener("click",function(e){
+                const parentDiv = e.target.parentElement
+                parentDiv.remove();
+            })
 
         // giving class
-        pageContent.setAttribute("class","pageContent")
-        detailsBtn.setAttribute("class","detailsBtn")
-        titel.setAttribute("class","titel")
-        deleteBtn.setAttribute("class","detailsBtn")
+        pageContent.setAttribute("class", "pageContent");
+        detailsBtn.setAttribute("class", "detailsBtn");
+        titel.setAttribute("class", "titel");
+        deleteBtn.setAttribute("class", "deleteBtn");
 
         // appending childs
-        pageContent.appendChild(detailsBtn)
-        pageContent.appendChild(titel)
-        pageContent.appendChild(deleteBtn)
-        mainContent.appendChild(pageContent)
+        pageContent.appendChild(detailsBtn);
+        pageContent.appendChild(titel);
+        pageContent.appendChild(deleteBtn);
+        mainContent.appendChild(pageContent);
     });
+}
+
+function clearInput() {
+    document.querySelector("#dateInput").value = "";
+    document.querySelector("#priorityInput").value = "";
+    document.querySelector("#discriptionInput").value = "";
 }
 
 export default addContent;
